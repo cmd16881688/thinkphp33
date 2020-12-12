de2150f36a2bb74df27ef1ffabe5e4b78265dc14
cmd16881688 <cmd16881688@outlook.com>
2020-12-13 02:25:27 +0800

    b1

--- ThinkPHP/ThinkPHP1.php
+++ ThinkPHP/ThinkPHP1.php
@@ -73,7 +73,7 @@ if(version_compare(PHP,'5.4.0','<')) {
 define('IS_CGI',(0 === strpos(PHP_SAPI,'cgi') || false !== strpos(PHP_SAPI,'fcgi')) ? 1 : 0 );
 define('IS_WIN',strstr(PHP_OS, 'WIN') ? 1 : 0 );
 define('IS_CLI',PHP_SAPI=='cli'? 1   :   0);
-
+a1
 if(!IS_CLI) {
     // 当前文件名
     if(!defined('_PHP_FILE_')) {
